97d5cf94d927269d3eb0ae3b305ba9038afdb956
Jayson Lennon <jayson@jaysonlennon.dev>
2026-04-20 14:42:01 -0700

    move tests to right spot

--- crates/ss-preview/src/frame_index.rs
+++ crates/ss-preview/src/frame_index.rs
@@ -24,3 +24,39 @@ pub fn frame_index_to_time(index: usize, fps: u32) -> f64 {
 pub fn total_frames(fps: u32, duration: f64) -> usize {
     (duration * fps as f64).floor() as usize
 }
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(0.0, 30, 10.0, Some(0))]
+    #[case(1.0, 30, 10.0, Some(30))]
+    #[case(9.999, 30, 10.0, Some(299))]
+    #[case(-1.0, 30, 10.0, None)]
+    #[case(-0.001, 30, 10.0, None)]
+    #[case(10.0, 30, 10.0, None)]
+    #[case(15.0, 30, 10.0, None)]
+    fn time_to_frame_index(
+        #[case] time: f64,
+        #[case] fps: u32,
+        #[case] duration: f64,
+        #[case] expected: Option<usize>,
+    ) {
+        assert_eq!(super::time_to_frame_index(time, fps, duration), expected);
+    }
+
+    #[rstest]
+    #[case(0, 30, 0.0)]
+    #[case(30, 30, 1.0)]
+    fn frame_index_to_time(#[case] index: usize, #[case] fps: u32, #[case] expected: f64) {
+        assert_eq!(super::frame_index_to_time(index, fps), expected);
+    }
+
+    #[rstest]
+    #[case(30, 10.0, 300)]
+    #[case(30, 10.5, 315)]
+    fn total_frames(#[case] fps: u32, #[case] duration: f64, #[case] expected: usize) {
+        assert_eq!(super::total_frames(fps, duration), expected);
+    }
+}
